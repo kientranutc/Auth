fcdb1ff98ba81172770b5b1065b90d5407cead87
kientranutc <kienkienutc95@gmail.com>
2018-06-12 11:53:13 +0700

    auth

---
+++ app/Http/Controllers/Backend/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+    public function  processAuth(LoginRequest $request)
+    {
+        
+
+    }
+}
